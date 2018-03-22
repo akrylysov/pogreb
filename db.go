b77bc72d746fd0a6fe1f01b555e7dde7f969b5d3
Joshua Olson <0joshuaolson1@gmail.com>
2018-03-21 19:04:16 -0600

    Make db.sync public (#13)

--- db.go
+++ db.go
@@ -130,9 +130,7 @@ func (db *DB) startSyncer(interval time.Duration) {
 			default:
 				modifications := db.metrics.Puts.Value() + db.metrics.Dels.Value()
 				if modifications != lastModifications {
-					db.mu.Lock()
-					db.sync()
-					db.mu.Unlock()
+					db.Sync()
 					lastModifications = modifications
 				}
 				time.Sleep(interval)
@@ -303,7 +301,10 @@ func (db *DB) Items() *ItemIterator {
 	return &ItemIterator{db: db}
 }
 
-func (db *DB) sync() error {
+// Sync commits the contents of the database to the backing FileSystem; this is effectively a noop for an in-memory database. It must only be called while the database is opened.
+func (db *DB) Sync() error {
+	db.mu.Lock()
+	defer db.mu.Unlock()
 	if err := db.data.Sync(); err != nil {
 		return err
 	}
